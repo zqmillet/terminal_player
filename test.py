89cc1749ffebe0111d763bf55e5dea85b0c20908
zqmillet <zqmillet@qq.com>
2019-03-11 23:09:14 +0800

    modify the file test.py

--- test.py
+++ test.py
@@ -5,14 +5,17 @@ import cv2
 def get_window_size():
     return shutil.get_terminal_size()
 
+def image_to_ascii(image, width = None, threshold_gray = 3, height_width_rate = 2.2):
+    if width is None:
+        width, _ = get_window_size()
+    pixels_in_char = image.shape[1] / width
 
-def image_to_ascii(image, pixels_in_char = 8, threshold_gray = 3, height_width_rate = 7/4):
     chars = numpy.asarray(list(' .,:;irsXA253hMHGS#9B&@'))
-    size = (round(image.shape[0] * height_width_rate / pixels_in_char), round(image.shape[1] / pixels_in_char) )
+    size = width, round(image.shape[0] / height_width_rate / pixels_in_char)
     image = numpy.sum(cv2.resize(image, size), axis = 2)
     image -= image.min()
     image = (1.0 - image / image.max())**threshold_gray * (chars.size - 1)
-    return '\n'.join( (''.join(r) for r in chars[image.astype(int)]))
+    return '\n'.join((''.join(r) for r in chars[image.astype(int)]))
 
 def testcases():
     image = cv2.imread('./videos/JZM1947.jpg')
