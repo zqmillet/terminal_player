c533cf82282f256e2af0da6ec9062cc0273a73f6
zqmillet <zqmillet@qq.com>
2019-03-12 23:14:39 +0800

    rename file

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import cv2
+import time
+
+from decode import decode
 
 video_capture = cv2.VideoCapture('./videos/plus1s.mp4')
+frame_per_second = video_capture.get(cv2.CAP_PROP_FPS)
 
 while True:
+    now = time.time()
     success, image = video_capture.read()
     if not success:
         break
 
-    import pdb; pdb.set_trace()
+    print(decode(image))
+    sleep_time = 1/frame_per_second - (time.time() - now)
+    time.sleep(sleep_time)
