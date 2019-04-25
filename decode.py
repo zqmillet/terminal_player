6dbce63a0d705a441c4c66355de4bdaeba1331ae
zqmillet <zqmillet@qq.com>
2019-04-25 23:55:49 +0800

    modify the file decode.py

--- decode.py
+++ decode.py
@@ -3,34 +3,29 @@ import numpy
 import cv2
 
 class Symbols:
-    A = '▀▄'
-    B = '██'
-    C = '▄▀'
-    S0000 = '  '
-    S0001 = ' ▄'
-    S0010 = '▄ '
-    S0011 = ' ▀'
-    S000 = ' ▀'
-    G = '  '
-    D = '▀ '
-    E = ' '
-    F = '▄ '
-    G = ' ▄ '
 
 def get_window_size():
     return shutil.get_terminal_size()
 
-def decode(image, width = None, threshold_gray = 3, height_width_rate = 2.2):
-    if width is None:
-        width, _ = get_window_size()
-    pixels_in_char = image.shape[1] / width
+class Decoder(object):
+    gray_chars = numpy.asarray(list(' .,:;irsXA253hMHGS#9B&@'))
+    threshold_gray = 3
+    height_width_rate = 2.2
 
-    chars = numpy.asarray(list(' .,:;irsXA253hMHGS#9B&@'))
-    size = width, round(image.shape[0] / height_width_rate / pixels_in_char)
-    image = numpy.sum(cv2.resize(image, size), axis = 2)
-    image -= image.min()
-    image = (1.0 - image / image.max())**threshold_gray * (chars.size - 1)
-    return '\n'.join((''.join(r) for r in chars[image.astype(int)])), image.shape[0]
+    def decode_with_chars(image, width = None):
+        if width is None:
+            width, _ = get_window_size()
+        pixels_in_char = image.shape[1] / width
+
+        chars =
+        size = width, round(image.shape[0] / self.height_width_rate / pixels_in_char)
+        image = numpy.sum(cv2.resize(image, size), axis = 2)
+        image -= image.min()
+        image = (1.0 - image / image.max())**self.threshold_gray * (chars.size - 1)
+        return '\n'.join((''.join(r) for r in chars[image.astype(int)])), image.shape[0]
+
+    def decode_with_blocks(image, width = None):
+        pass
 
 def testcases():
     image = cv2.imread('./videos/JZM1947.jpg')
